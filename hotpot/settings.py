1cfa8e0d7985ec4e12dc1dfb54d5093761b446d6
Darjan Salaj <salaj@student.tugraz.at>
2016-03-03 08:42:13 +0100

    try switch to pg

--- hotpot/settings.py
+++ hotpot/settings.py
@@ -84,13 +84,15 @@ WSGI_APPLICATION = 'hotpot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
